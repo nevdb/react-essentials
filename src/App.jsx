107dd9d8a547ad8bb721bd1f876951f481a740c9
nevena <dnevena27@gmail.com>
2025-11-21 17:08:38 +0200

    Component composition

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header";
 import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,12 +17,23 @@ function App() {
           count is {count}
         </button>
 
-        <h2>Core Concepts</h2>
-        <ul class="flex gap-4">
-          <CoreConcept {...CORE_CONCEPTS[0]} />
-          <CoreConcept {...CORE_CONCEPTS[1]} />
-          <CoreConcept {...CORE_CONCEPTS[2]} />
-        </ul>
+        <section>
+          <h2>Core Concepts</h2>
+          <ul class="flex gap-4">
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+          </ul>
+        </section>
+        <section id="examples" class="my-20">
+          <h2>Examples</h2>
+          <menu class="flex justify-center">
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
       </div>
     </>
   );
